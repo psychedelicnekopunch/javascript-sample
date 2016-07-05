8d0708b48ee757f24aa2d720d17cb3bbf691368e
masa69 <psychedelic.nekopunch@gmail.com>
2016-07-06 01:10:36 +0900

    if

--- if.js
+++ if.js
@@ -19,3 +19,44 @@ if (i++ > 2) {
 }
 
 console.log('i is ', i);
+
+
+var func = function()
+{
+	console.log('call');
+};
+
+var obj = {
+	func: function()
+	{
+		console.log('call');
+	},
+};
+
+if (typeof func) {
+	console.log('func is', typeof func);
+	if (typeof func === 'function') {
+		console.log('func: success');
+	}
+}
+
+if (typeof invalidFunc) {
+	console.log('invalidFunc is', typeof invalidFunc);
+	if (typeof invalidFunc === 'undefined') {
+		console.log('invalidFunc: success');
+	}
+}
+
+if (typeof obj) {
+	console.log('obj is', typeof obj);
+	if (typeof obj === 'object') {
+		console.log('obj: success');
+	}
+}
+
+if (typeof obj.func) {
+	console.log('obj.func is', typeof obj.func);
+	if (typeof obj.func === 'function') {
+		console.log('obj.func: success');
+	}
+}
