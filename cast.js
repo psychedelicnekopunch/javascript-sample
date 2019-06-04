65afa09ada8ccb94b34c4efb1c27afe9cf0fc57c
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-06-04 16:40:27 +0900

    update

--- cast.js
+++ cast.js
@@ -12,3 +12,27 @@ console.log((f)) // 'false'
 
 console.log((t == 'true')) // true
 console.log((f == 'true')) // false
+
+
+function parseBool(value) {
+	switch (value) {
+	case true:
+	case 'true':
+	case 1:
+	case '1':
+		return true;
+	case false:
+	case 'false':
+	case 0:
+	case '0':
+		return false;
+	default:
+		return null;
+	}
+}
+
+console.log(parseBool(t))
+console.log(parseBool(f))
+
+console.log(parseBool(t).toString())
+console.log(parseBool(f).toString())
