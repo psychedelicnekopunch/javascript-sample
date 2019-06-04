e89637bf93b6d4520fbfd97152dab13a6232c218
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-06-04 13:10:29 +0900

    add

---
+++ cast.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// string to boolean
+const t = 'true'
+const f = 'false'
+
+console.log(Boolean(t)) // true
+console.log(Boolean(f)) // true
+
+console.log((t)) // 'true'
+console.log((f)) // 'false'
+
+console.log((t == 'true')) // true
+console.log((f == 'true')) // false
