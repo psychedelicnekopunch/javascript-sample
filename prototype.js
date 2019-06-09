9e722b90f3143368aab97e0715ab6e4ed404ceaa
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-06-10 01:57:47 +0900

    fix

--- prototype.js
+++ prototype.js
@@ -2,13 +2,20 @@
 
 
 function obj() {
+	var str = 'undefined'
 	this.test = 2
+	/*return {
+		get2: () => {
+			return this.test;
+		}
+	}*/
 }
 
 obj.prototype = {
 	get: function(func) {
 		console.log('\n' + func + '(): function() {}')
 		console.log(this)
+		// console.log(str) // undefined
 		return this.test
 	},
 	set: function(func, x) {
@@ -38,3 +45,5 @@ test2.get('test2')
 
 console.log(test.test)
 console.log(test2.test)
+
+// console.log(test.get2())
