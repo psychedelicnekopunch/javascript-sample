8039a2ff316ce8285c93bdd6b3a9153766cd6efb
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-06-25 16:09:37 +0900

    add

---
+++ foreach.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+
+const arr = ['aaa', 'bbbb', 'cccc', 'dddd', 'eeee']
+
+arr.forEach((value, key) => {
+	console.log(key + '. ' + value)
+})
+
+
+const obj = {
+	id: 0,
+	name: 'psychedelic nekopunch',
+	address: 'Tokyo',
+	sex: 'male',
+	age: 20,
+}
+
+
+// オブジェクトは forEach() メソッドを持っていないから使えない
+// obj.forEach((value, key) => {
+// 	console.log(key + '. ' + value)
+// })
+
+const keys = Object.keys(obj)
+const values = Object.values(obj)
+
+values.forEach((value, i) => {
+	console.log(keys[i] + '. ' + value)
+})
+
+const cnt = keys.length
+for (let i = 0; i < cnt; i++) {
+	console.log(keys[i] + '. ' + values[i])
+}
