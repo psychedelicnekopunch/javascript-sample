9c8cb373b992a5239c2c065137075aa631711bdb
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2020-10-06 02:05:16 +0900

    add

---
+++ float.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let i = 1.7
+console.log(i)
+console.log(Number(i))
+console.log(Math.round(i))
+console.log(Math.ceil(i))
+console.log(Math.floor(i))
+
+console.log("=========")
+
+i = 1.4
+console.log(i)
+console.log(Number(i))
+console.log(Math.round(i))
+console.log(Math.ceil(i))
+console.log(Math.floor(i))
