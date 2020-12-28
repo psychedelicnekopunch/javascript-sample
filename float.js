825a9d2a17555f6e903f54bf89a9a1840407d9aa
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2020-12-28 11:10:08 +0900

    update

--- float.js
+++ float.js
@@ -15,3 +15,23 @@ console.log(Number(i))
 console.log(Math.round(i))
 console.log(Math.ceil(i))
 console.log(Math.floor(i))
+
+
+let depth = function (d) {
+	let res = 1
+	for (i = 0; i < d; i++) {
+		res = res * 10
+	}
+	return res
+}
+
+let log = function (i, d) {
+	console.log("=========")
+	console.log(Math.round(i * d) / d)
+	console.log(Math.ceil(i * d) / d)
+	console.log(Math.floor(i * d) / d)
+}
+
+log(1.748, depth(0))
+log(1.748, depth(1))
+log(1.748, depth(2))
