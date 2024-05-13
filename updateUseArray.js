1a8e6e3c667be3340ea3e2802569e2f153589d23
masa <psychedelic.nekopunch@gmail.com>
2024-05-13 11:59:51 +0900

    update

--- updateUseArray.js
+++ updateUseArray.js
@@ -5,6 +5,10 @@ function change1(a, i) {
 
 function change2(a) {
 	a.push('test2')
+	a.push('test2')
+	a.push('test2')
+	a.push('test2')
+	a[0] = 'test3'
 }
 
 let arr = ['apple', 'banana', 'candy']
@@ -18,5 +22,7 @@ console.log(arr)
 
 let copy = arr
 
+change2(arr)
 change1(copy, 1)
 console.log(arr)
+console.log(copy)
