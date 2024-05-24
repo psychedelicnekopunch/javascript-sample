38b708f9474c5698523d160fd56be242cc49057c
masa <psychedelic.nekopunch@gmail.com>
2024-05-24 12:59:26 +0900

    add

--- replace_text.js
+++ replace_text.js
@@ -1,3 +1,17 @@
 const str  = 'aaaa_bbbb_ccccc_ddddd_eeeee_fffff';
 console.log(str.replace('_', '-'))
 console.log(str.replace(/_/g, '-'))
+
+
+
+function ajust(str) {
+	console.log(str.toLowerCase().replace(' ', '-'))
+}
+
+
+ajust('Visa')
+ajust('MasterCard')
+ajust('Diners Club')
+ajust('Discover')
+ajust('JCB')
+ajust('American Express')
