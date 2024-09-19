646c647895497d098b34ddcedbd4acbf091bf4ec
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-09-19 17:23:33 +0900

    update

--- code_test.js
+++ code_test.js
@@ -17,7 +17,13 @@ while (true) {
 		break
 	}
 	res2 = `${res2}${arr[i]}`
+	// 最後尾を削除
 	arr.pop()
+
+	// slice(start, end)
+	// start: 0からはじまるインデックス。抽出を始める位置を指定する
+	// end: 0からはじまるインデックス。抽出に含まず、その直前までを抽出する
+	// arr = arr.slice(0, i)
 }
 
 console.log(res)
